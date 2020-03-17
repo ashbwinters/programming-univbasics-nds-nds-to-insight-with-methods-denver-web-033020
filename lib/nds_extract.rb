efbf2374eda4d4f9d75c0bf1d0a8a1d7c79392f1
Ashley Winters <ashbwinters@gmail.com>
2020-03-17 22:58:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,24 +21,3 @@ def directors_totals(nds)
   
   return result
 end
-
-
-
-def directors_totals(nds)
-  directors_total_worldwide_gross = {}
-  outer_index = 0
-  
-  while outer_index < nds.length do
-    inner_index = 0
-    directors_gross = 0
-    while inner_index < nds[outer_index][:movies].length do
-      directors_gross += nds[outer_index][:movies][inner_index][:worldwide_gross]
-      inner_index += 1 
-    end
-    directors_total_worldwide_gross[nds[outer_index][:name]] = directors_gross
-    outer_index += 1
-  end 
-  
-  return directors_total_worldwide_gross
-  
-end
